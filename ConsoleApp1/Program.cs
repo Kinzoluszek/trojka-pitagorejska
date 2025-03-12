5829666fe69c0065d1f81d3370d68171709a5a1b
4763007 <4763007@TE-GDA-D-D21E16>
2025-03-12 11:45:16 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Wpisz 3 liczby naturalne");
+int[] liczby = new int[3];
+for (int i = 0; i < 3; i++)
+{
+    bool isValid = false;
+    while (!isValid)
+    {
+        Console.Write($"Liczba {i + 1}: ");
+string input = Console.ReadLine();
+isValid = int.TryParse(input, out liczby[i]);
+if (!isValid)
+{
+    Console.WriteLine("Nieprawidłowy format liczby. Spróbuj ponownie.");
+}
+                }
+            }
+
+            Console.WriteLine("Wprowadzone liczby to:");
+Array.Sort(liczby);
+foreach (int liczba in liczby)
+{
+    Console.WriteLine(liczba);
+}
+int a = liczby[0];
+int b = liczby[1];
+int c = liczby[2];
+if (a *a + b *b == c* c)
+    Console.WriteLine("Spelniaja trojke pitagorejska");
+else Console.WriteLine("Nie spelniaja trojki pitagorejskiej");
